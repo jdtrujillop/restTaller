13d55be305869f1e54b7ee542fe80f014ba3a238
sala_c <sala_c@10.180.12.43>
2016-11-05 11:24:23 -0500

    asdfasdfa

--- src/main/java/example/microservice/micro.java
+++ src/main/java/example/microservice/micro.java
@@ -1,11 +1,22 @@
 package example.microservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(name="miServicio")
 public class micro {
+	private List<Placa> listPlaca;
+	public micro(){
+		listPlaca = new ArrayList<>();
+	    listPlaca.add(new Placa("AAA-123","BOGOTA"));
+	    listPlaca.add(new Placa("BBB-123","MEDELLIN"));
+	    listPlaca.add(new Placa("CCC-123","CALI"));
+		
+	}
 
 	@WebMethod()
 	public String sayHello(@WebParam(name="nombre") String name) {
@@ -17,6 +28,23 @@ public class micro {
 	public int sumar(@WebParam(name="valor1") int a,@WebParam(name="valor2") int b) {
 	    System.out.println("sumar: " + (a+b));
 	    return (a+b);
+	}
+	
+	@WebMethod()
+	public List<Placa> listarPlacasRobadas() {
+	    return listPlaca;
+	}
+	
+	@WebMethod()
+	public boolean verificarPlaca(@WebParam(name="numPlaca")String numPlaca) {
+		
+		for (Placa placa : listPlaca) {
+			if(placa.getNumPlaca().equalsIgnoreCase(numPlaca)){
+				return true;
+			}
+		}
+		
+	    return false;
 	}
 	
 	
